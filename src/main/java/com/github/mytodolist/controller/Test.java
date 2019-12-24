e1c310ea052f4961c464ce1a26c8649247a1d075
CreativeLDeng <dengliu-foxmail.com>
2019-12-24 16:39:56 +0800

    添加swagger2接口文档配置

--- src/main/java/com/github/mytodolist/controller/Test.java
+++ src/main/java/com/github/mytodolist/controller/Test.java
@@ -1,15 +1,24 @@
 package com.github.mytodolist.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/")
+@Api(tags = "测试接口")
 public class Test {
 
     @RequestMapping("test")
-    public String test() {
+    @ApiOperation(value = "测试接口", notes = "接口描述", httpMethod = "GET")
+    @ResponseBody
+    public String testApi() {
+        return "apireturn";
+    }
+
+    @RequestMapping(value = "testPage",method = RequestMethod.GET)
+    public String testPage() {
         return "index.html";
     }
 }
